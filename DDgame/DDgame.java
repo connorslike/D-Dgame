6656424f22e415aa386dd8235207abb9023c1841
Connor Slike <slike@hawaii.edu>
2018-06-27 09:41:24 -1000

    Initial setup
    Initial setup

---
+++ DDgame/DDgame.java
@@ -0,0 +1,11 @@
+
+public class DDgame {
+	  
+	public static void main(String args[]) {
+	
+		Intro intro = new Intro();
+
+		intro.introText();
+	}
+}
+
---
+++ DDgame/Intro.java
@@ -0,0 +1,15 @@
+public class Intro {
+	public static int introText(){
+		int choice = 0;
+	
+		System.out.println("\n\n Welcome to command line dungeons and dragons!!! \n\n");
+	
+		System.out.println("(1) Host Game \n");
+		System.out.println("(2) Join Game \n");
+		System.out.println("(3) Create Dungeon Levels\n");
+		System.out.println("(4) Quit\n");
+		System.out.println("\n");
+		
+		return choice;
+	}
+}
